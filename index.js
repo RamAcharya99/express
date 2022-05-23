f72acbb8056ed6e6f0899d0a19500eafe887630a
RamAcharya99 <izaki.ram99@gmail.com>
2022-05-23 12:10:43 +0545

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+console.log(express);
+
+// git install
+// add git cmd to environmental variables
+
+// git init
+
+//git remote add origin https://github.com/RamAcharya99/express
+
+// to check git remote -v
+
+//git pull origin <brach_name>
+//git pull origin master
+
+
+
+// git clone https://github.com/RamAcharya99/express
+
+
+
